e2929531a47e57d3eb9c2a81ea4ddafd4550ed2b
ayesha370 <122480244+ayesha370@users.noreply.github.com>
2024-06-02 17:39:43 +0500

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.protect = async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'You are not logged in' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return res.status(401).json({ message: 'The user belonging to this token no longer exists' });
+    }
+
+    // Grant access to protected route
+    req.user = currentUser;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
